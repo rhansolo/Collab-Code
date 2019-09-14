00b67a153a002af98bf8279730202b54544ca3a4
rhansolo <robin.han.501@gmail.com>
2019-09-14 16:54:54 -0400

    .

--- working/asdf_91a318f7dbf8787e4c98a42023db7d/Solution.java
+++
@@ -1,5 +0,0 @@
-public class Solution {
-    public static void main(String[] args) throws InterruptedException{
-        System.out.println(5);
-    }
-}
--- working/asdf_f6f8b609b40771f19fec1613dd441a/Solution.java
+++
@@ -1,5 +0,0 @@
-public class Solution {
-    public static void main(String[] args) {
-        System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
-    }
-}
